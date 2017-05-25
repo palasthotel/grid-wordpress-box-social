210c8174069578236c8f8b96a19a2d8633cf119f
Philipp Zedler <philipp@neue-musik.com>
2017-05-25 16:00:09 +0200

    allow overriding the facebook post template

--- grid_facebook_box/grid_facebook_feed_box.php
+++ grid_facebook_box/grid_facebook_feed_box.php
@@ -97,31 +97,11 @@ class grid_facebook_feed_box extends grid_list_box {
 	public function get_post($item, $page = ""){
 		$parts = explode( "_", $item->id );
 		ob_start();
-		?>
-		<div
-				id="grid-facebook-post-<?php echo $item->id; ?>"
-				class="fb-post"
-				data-href="https://www.facebook.com/<?php echo $parts[0] ?>/posts/<?php echo $parts[1]; ?>/"
-				data-show-text="true"
-		>
-			<blockquote
-					cite="https://www.facebook.com/<?php echo $parts[0] ?>/posts/<?php echo $parts[1]; ?>/"
-					class="fb-xfbml-parse-ignore">
-				<?php if("" != $page): ?>
-				Posted by <a
-						href="https://www.facebook.com/facebook/"><?php echo $page; ?></a>
-				on&nbsp;<a
-						href="https://www.facebook.com/20531316728/posts/10154009990506729/"><?php echo $item->created_time; ?></a>
-				<?php endif; ?>
-			</blockquote>
-		</div>
-		<script>
-			document.addEventListener("DOMContentLoaded", function () {
-				var post = document.getElementById("grid-facebook-post-<?php echo $item->id; ?>");
-				post.setAttribute("data-width", post.parentNode.clientWidth);
-			});
-		</script>
-		<?php
+		if($overridden_template = locate_template("grid/facebook-post.tpl.php")){
+			include $overridden_template;
+		} else {
+			require plugin_dir_path(__FILE__)."../templates/facebook-post.tpl.php";
+		}
 		$output = ob_get_contents();
 		ob_end_clean();
 		return $output;
