99eac2088dd58763d56a8d222ed81cb69f5626a0
Edward <me@edwardbock.de>
2017-07-25 16:46:24 +0200

    Update grid-social-boxes.php

--- grid-social-boxes.php
+++ grid-social-boxes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Grid Social Boxes
  * Plugin URI: https://github.com/palasthotel/wordpress-grid-box-social
  * Description: Some social network boxes. Facebook, Twitter, Instagram and Youtube.
- * Version: 1.4.3
+ * Version: 1.4.4
  * Author: Palasthotel <rezeption@palasthotel.de> (in person: Edward Bock, Enno Welbers)
  * Author URI: http://www.palasthotel.de
  * License: http://www.gnu.org/licenses/gpl-2.0.html GPLv2
