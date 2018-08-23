df808c16bb6eb98d28e69443c606efd7b45b5946
Edward <edward.bock@palasthotel.de>
2018-08-23 13:48:20 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,4 +1,4 @@
 <?php
 if (!defined('GRID_FACEBOOK_FIELDS')) {
-    define('GRID_FACEBOOK_FIELDS', 'message,created_time,id');
+    define('GRID_FACEBOOK_FIELDS', 'message,created_time,id,link,from,type,name,caption,description,picture');
 }
