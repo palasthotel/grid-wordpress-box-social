6c839c30b5de0536c57b19559acfe4bf9060a61a
EdwardBock <edward.bock@palasthotel.de>
2019-11-20 15:44:07 +0100

    fields changed with api version

--- settings.php
+++ settings.php
@@ -1,4 +1,4 @@
 <?php
 if (!defined('GRID_FACEBOOK_FIELDS')) {
-    define('GRID_FACEBOOK_FIELDS', 'message,created_time,id,link,from,type,name,caption,description,picture');
+    define('GRID_FACEBOOK_FIELDS', 'message,created_time,id,from,permalink_url,attachments{title,description},picture');
 }
