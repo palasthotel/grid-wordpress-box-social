da63f05f6de2472fd6d2c34d56572e9220629705
EdwardBock <edward.bock@palasthotel.de>
2019-12-17 17:36:41 +0100

    verskion number

--- grid-social-boxes.php
+++ grid-social-boxes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Grid Social Boxes
  * Plugin URI: https://github.com/palasthotel/wordpress-grid-box-social
  * Description: Some social network boxes. Facebook, Twitter, Instagram and Youtube.
- * Version: 1.4.11
+ * Version: 1.5.0
  * Author: Palasthotel <rezeption@palasthotel.de> (in
  * person: Edward Bock, Enno Welbers)
  * Author URI: http://www.palasthotel.de
